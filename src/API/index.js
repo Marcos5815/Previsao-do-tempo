d33b6dd1adefe422d650bcf7a3829ea1d94e5008
Marcos Silva <marcos5815mvs@gmail.com>
2026-01-01 18:16:18 -0300

    set production base URL

--- src/API/index.js
+++ src/API/index.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+const forProduction = import.meta.env.PROD;
+
+const baseURL = forProduction? 'https://api.hgbrasil.com' : '/api'
+
 export const HTTP = axios.create({
-    baseURL: '/api'
+    baseURL: baseURL
 })
